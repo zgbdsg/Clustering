c2404712db26d34d35fb714a2d5ab6d4b697caea
zgbdsg <zgbdsg@gmail.com>
2013-10-26 22:17:13 +0800

    second

--- Clustering/src/auxiliary/Kmeans.java
+++ Clustering/src/auxiliary/Kmeans.java
@@ -4,9 +4,12 @@
  */
 package auxiliary;
 
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  *
- * @author daq
+ * @author zhouguobing
  */
 public class Kmeans {
 
@@ -21,7 +24,56 @@ public class Kmeans {
      * clusterIndex[i] should store the cluster index which the ith sample belongs to
      */
     public void train(double[][] features, int K, double[][] clusterCenters, int[] clusterIndex) {
+        Random random = new Random(features.length);
         
+        for(int i=0;i<K;i ++){
+        	int index = Math.abs(random.nextInt()%features.length);
+        	System.out.println(index);
+        	clusterCenters[i] = features[index];
+        }
+        
+        double[][] distances = new double[K][features.length];
+        
+        for(int i=0;i < K;i ++){
+        	for(int j=0;j< features.length;j ++){
+        		distances[i][j] = getDistance(clusterCenters[i], features[j]);
+        	}
+        }
+        
+        clusterIndex = updateIndex(distances);
     }
     
+    double getDistance(double[] center,double[] point){
+    	double sum =0;
+    	int len = center.length;
+    	
+    	for(int i=0;i<len;i ++){
+    		double diff = center[i] - point[i];
+    		sum +=Math.pow(diff, 2);
+    	}
+    	
+    	return sum;
+    }
+    
+    int[] updateIndex(double[][] distances){
+    	int[] clusterindex = new int[distances.length];
+    	
+    	return clusterindex;
+    }
+    
+    double[] getRow(int rowindex,double[][] data){
+    	double[] row = new double[data[0].length];
+    	
+    	for(int i=0;i<data.length;i ++){
+    		row[i] = data[i][rowindex];
+    	}
+    	
+    	return row;
+    }
+    
+    int minIndex(double[] row){
+    	int min = 0;
+    	
+    	for(int i=0;i)
+    }
 }
